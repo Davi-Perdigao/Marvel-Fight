7984861b6db21db1125e28e53fde26a1cf58b22d
Davi Perdigão <davivcperdigao@gmail.com>
2022-05-19 08:25:19 -0300

    debug e gif

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,8 @@
-/* 
- OBJETIVO 1 - quando passar o mouse em cima do personagem temos que:
-
-  - colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele 
-
- - retirar a classe selecionado do personagem que está selecionado
-
-
- OBJETIVO 2 - quando passar o mouse em cima do personagem, trocar a imagem e nome do personagem grande
-
-    - alterar a imagem do jogador 1
-    - alterar o nome do jogador 1
-*/
+// - classe selecionado no personagem que passamos o mouse em cima para adicionar a animação  
+// - retira a classe selecionado do personagem que está selecionado
+// - quando passar o mouse em cima do personagem, trocar a imagem e nome do personagem grande
+// - alterar a imagem do jogador 1
+// - alterar o nome do jogador 1
 
 const personagens = document.querySelectorAll(".personagem");
 
@@ -21,24 +13,20 @@ personagens.forEach((personagem) => {
 
     if(idSelecionado === 'ultron') return;
 
-    /* 
-        OBJETIVO 1 - quando passar o mouse em cima do personagem temos que:
+    
+      // - classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele 
 
-        - colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele 
-
-        - retirar a classe selecionado do personagem que está selecionado
-    */
+      // - retirar a classe selecionado do personagem que está selecionado
+    
     const personagemSelecionado = document.querySelector(".selecionado");
     personagemSelecionado.classList.remove("selecionado");
 
     personagem.classList.add("selecionado");
-
-    /* 
-        OBJETIVO 2 - quando passar o mouse em cima do personagem da listagem, trocar a imagem e nome do personagem grande
-            - alterar a imagem do jogador 1
-            - alterar o nome do jogador 1
-    */
-
+    
+        // - quando passar o mouse em cima do personagem da listagem, trocar a imagem e nome do personagem grande
+        // - alterar a imagem do jogador 1
+        // - alterar o nome do jogador 1
+    
     const imagemJogador1 = document.getElementById('personagem-jogador-1');
     imagemJogador1.src = `./src/images/${idSelecionado}.png`;
 
